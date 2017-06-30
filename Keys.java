924a24d1fced85c1eb03c45e9dba6a6134d3f2ca
FrancisLawlor <franlawlor0@gmail.com>
2017-06-30 23:10:58 +0100

    Added initial classes and json keys as constants.

---
+++ Keys.java
@@ -0,0 +1,19 @@
+public class Keys {
+    private static final String ERROR_CODE = "error_code";
+    private static final String ERROR_MESSAGE = "error_message";
+    private static final String NUMBER_OF_RESULTS = "numberofresults";
+    private static final String TIMESTAMP = "timestamp";
+    private static final String RESULTS = "results";
+    private static final String STOP_ID = "stopid";
+    private static final String DISPLAY_STOP_ID = "displaystopid";
+    private static final String SHORT_NAME = "shortname";
+    private static final String SHORT_NAME_LOCALIZED = "shortnamelocalized";
+    private static final String FULL_NAME = "fullname";
+    private static final String FULL_NAME_LOCALIZED = "fullnamelocalized";
+    private static final String LATITUDE = "latitude";
+    private static final String LONGITUDE = "longitude";
+    private static final String LAST_UPDATED = "lastupdated";
+    private static final String OPERATORS = "operators";
+    private static final String NAME = "name";
+    private static final String ROUTES = "routes";
+}
