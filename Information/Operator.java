326ffb2b492ffa26ad23573852f09c95f228709e
FrancisLawlor <franlawlor0@gmail.com>
2017-07-03 19:22:24 +0100

    Implemented Result class.

--- Information/Operator.java
+++ Information/Operator.java
@@ -2,12 +2,16 @@ package Information;
 
 import Interfaces.IOperator;
 
+import javax.json.JsonObject;
 import java.util.ArrayList;
 
 /**
  * Created by francis on 03/07/17.
  */
 public class Operator implements IOperator {
+    public Operator(JsonObject jsonObject) {
+    }
+
     @Override
     public String getName() {
         return null;
--- Information/Result.java
+++ Information/Result.java
@@ -3,63 +3,90 @@ package Information;
 import Data.Keys;
 import Interfaces.IResult;
 
+import javax.json.Json;
 import javax.json.JsonObject;
+import javax.json.JsonReader;
+import javax.json.JsonValue;
+import java.io.StringReader;
 import java.util.ArrayList;
 
 public class Result implements IResult {
-    private JsonObject data;
+    private String stopId;
+    private String displayStopId;
+    private String shortName;
+    private String shortNameLocalized;
+    private String fullName;
+    private String fullNameLocalized;
+    private String latitude;
+    private String longitude;
+    private String lastUpdated;
+    private ArrayList<Operator> operators = new ArrayList<Operator>();
 
     Result(JsonObject resultData) {
-        data = resultData;
+        this.stopId = resultData.getJsonString(Keys.STOP_ID).toString();
+        this.displayStopId = resultData.getJsonString(Keys.DISPLAY_STOP_ID).toString();
+        this.shortName = resultData.getJsonString(Keys.SHORT_NAME).toString();
+        this.shortNameLocalized = resultData.getJsonString(Keys.SHORT_NAME_LOCALIZED).toString();
+        this.fullName = resultData.getJsonString(Keys.FULL_NAME).toString();
+        this.fullNameLocalized = resultData.getJsonString(Keys.FULL_NAME_LOCALIZED).toString();
+        this.latitude = resultData.getJsonString(Keys.LATITUDE).toString();
+        this.longitude = resultData.getJsonString(Keys.LONGITUDE).toString();
+        this.lastUpdated = resultData.getJsonString(Keys.LAST_UPDATED).toString();
+
+        for (JsonValue operatorData: resultData.getJsonArray(Keys.OPERATORS)) {
+            JsonReader jsonReader = Json.createReader(new StringReader(operatorData.toString()));
+            operators.add(new Operator(jsonReader.readObject()));
+            jsonReader.close();
+        }
     }
 
     @Override
     public String getStopId() {
-        return data.getJsonString(Keys.STOP_ID).toString();
+        return this.stopId;
     }
 
     @Override
     public String getDisplayStopId() {
-        return null;
+        return this.displayStopId;
     }
 
     @Override
     public String getShortName() {
-        return null;
+        return this.shortName;
     }
 
     @Override
     public String getShortNameLocalized() {
-        return null;
+        return this.shortNameLocalized;
     }
 
     @Override
     public String getFullName() {
-        return null;
+        return this.fullName;
     }
 
     @Override
     public String getFullNameLocalized() {
-        return null;
+        return this.fullNameLocalized;
     }
 
     @Override
     public String getLatitude() {
-        return null;
+        return this.latitude;
     }
 
     @Override
     public String getLongitude() {
-        return null;
+        return this.longitude;
     }
 
     @Override
     public String getLastUpdated() {
-        return null;
+        return this.lastUpdated;
     }
 
     @Override
     public ArrayList<Operator> getOperators() {
-        return null;
+        return this.operators;
     }
 }
